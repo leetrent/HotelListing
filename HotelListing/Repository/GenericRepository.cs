b7fb51ee5637bf70c398d6d05ed41da791c5b966
LeeTrent <lee.trent.1@gmail.com>
2021-10-05 17:40:07 -0500

    Adds DB repositories and Unit of Work

---
+++ HotelListing/Repository/GenericRepository.cs
@@ -0,0 +1,89 @@
+﻿using HotelListing.Data;
+using HotelListing.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HotelListing.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DatabaseContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public async Task Delete(int id)
+        {
+            T entity = await _dbSet.FindAsync(id);
+            _dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (includes != null)
+            {
+                foreach (var includePropery in includes)
+                {
+                    query = query.Include(includePropery);
+                }
+            }
+
+            return await query.AsNoTracking().FirstOrDefaultAsync(expression);
+        }
+
+        public async Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (expression != null)
+            {
+                query = query.Where(expression);
+            }
+
+            if (includes != null)
+            {
+                foreach (var includePropery in includes)
+                {
+                    query = query.Include(includePropery);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            return await query.AsNoTracking().ToListAsync();
+        }
+
+        public async Task Insert(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task InsertRange(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
