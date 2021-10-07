878e60ac492d25d9a8783df9cc407d3aafe4193e
LeeTrent <lee.trent.1@gmail.com>
2021-10-07 17:46:54 -0500

    Adds CountryController, implements HttpGet to return list of Countries

---
+++ HotelListing/Controllers/CountryController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using HotelListing.Data;
+using HotelListing.DTO;
+using HotelListing.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger<CountryController> _logger;
+
+        public CountryController(IUnitOfWork unitOfWork, IMapper mapper, ILogger<CountryController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+            try
+            {
+                IList<Country> entityList = await _unitOfWork.Countries.GetAll();
+                IList<CountryDTO> dtoList = _mapper.Map<IList<CountryDTO>>(entityList);
+
+                return Ok(dtoList);
+            }
+            catch (Exception exc)
+            {
+                _logger.LogError(exc.Message);
+                _logger.LogError(exc.StackTrace);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -1,5 +1,7 @@
 using HotelListing.Config;
 using HotelListing.Data;
+using HotelListing.IRepository;
+using HotelListing.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -41,13 +43,16 @@ namespace HotelListing
             // AUTOMAPPER
             services.AddAutoMapper(typeof(MapperConfig));
 
+            // UNIT OF WORK
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
             // SWAGGER
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1" });
             });
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(op => op.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
