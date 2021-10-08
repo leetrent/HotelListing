a43a9185e7588a4e671b50e2e6cdbbe23d198502
LeeTrent <lee.trent.1@gmail.com>
2021-10-08 10:52:24 -0500

    Adds get by id method to country controller

--- HotelListing/Controllers/CountryController.cs
+++ HotelListing/Controllers/CountryController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace HotelListing.Controllers
@@ -36,6 +37,41 @@ namespace HotelListing.Controllers
                 IList<CountryDTO> dtoList = _mapper.Map<IList<CountryDTO>>(entityList);
 
                 return Ok(dtoList);
+            }
+            catch (Exception exc)
+            {
+                _logger.LogError(exc.Message);
+                _logger.LogError(exc.StackTrace);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCountry(int id)
+        {
+            try
+            {
+                ////////////////////////////////////////////////////////////////////////////////////////////////////
+                // Version #1:
+                ////////////////////////////////////////////////////////////////////////////////////////////////////
+                Expression<Func<Country, bool>> expression = c => c.Id == id;
+                List<string> includes = new List<string> { "Hotels" };
+                Country entity = await _unitOfWork.Countries.Get(expression, includes);
+
+                ////////////////////////////////////////////////////////////////////////////////////////////////////
+                // Version #2:
+                ////////////////////////////////////////////////////////////////////////////////////////////////////
+                //Country entity = await _unitOfWork.Countries.Get(c => c.Id == id, new List<string> { "Hotels" });
+
+                ////////////////////////////////////////////////////////////////////////////////////////////////////
+                // Map Entity to DTO
+                ////////////////////////////////////////////////////////////////////////////////////////////////////
+                CountryDTO dto = _mapper.Map<CountryDTO>(entity);
+
+                ////////////////////////////////////////////////////////////////////////////////////////////////////
+                // Return ActionResult
+                ////////////////////////////////////////////////////////////////////////////////////////////////////
+                return Ok(entity);
             }
             catch (Exception exc)
             {
