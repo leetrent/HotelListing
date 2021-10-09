1e7d612046454463074f97f656eb6c4ef12c4e18
LeeTrent <lee.trent.1@gmail.com>
2021-10-09 18:09:27 -0500

    Completes implementation of user registration in account controller

--- HotelListing/Controllers/AccountController.cs
+++ HotelListing/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace HotelListing.Controllers
 {
@@ -15,22 +16,24 @@ namespace HotelListing.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<ApiUser> _userManager;
-        private readonly SignInManager<ApiUser> _signInManager;
         private readonly IMapper _mapper;
         private readonly ILogger<AccountController> _logger;
 
-        public AccountController(UserManager<ApiUser> userManager, SignInManager<ApiUser> signInManager, IMapper mapper, ILogger<AccountController> logger)
+        public AccountController(UserManager<ApiUser> userManager, IMapper mapper, ILogger<AccountController> logger)
         {
             _userManager = userManager;
-            _signInManager = signInManager;
             _mapper = mapper;
             _logger = logger;
         }
 
         [HttpPost]
+        [Route("register")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Register([FromBody] UserDTO userDTO)
         {
-            _logger.LogInformation($"Registration Attempt for {userDTO.Email} ");
+            _logger.LogInformation($"Registration attempt for {userDTO.Email} ");
 
             if (ModelState.IsValid == false)
             {
@@ -40,7 +43,8 @@ namespace HotelListing.Controllers
             try
             {
                 ApiUser userEntity = _mapper.Map<ApiUser>(userDTO);
-                IdentityResult identityResult = await _userManager.CreateAsync(userEntity);
+                userEntity.UserName = userEntity.Email;
+                IdentityResult identityResult = await _userManager.CreateAsync(userEntity, userDTO.Password);
 
                 if (identityResult.Succeeded == false)
                 {
@@ -52,8 +56,7 @@ namespace HotelListing.Controllers
                         ModelState.AddModelError(identityError.Code, identityError.Description);
                     }
 
-                    return BadRequest($"User registration attempt failed.");
-                    //return BadRequest(ModelState);
+                    return BadRequest(ModelState);
                 }
                 return Accepted();
             }
@@ -63,9 +66,41 @@ namespace HotelListing.Controllers
                 _logger.LogError(exc.Message);
                 _logger.LogError(exc.StackTrace);
 
-                return Problem($"Something Went Wrong in the {nameof(Register)}", statusCode: StatusCodes.Status500InternalServerError);
+                return Problem($"Exception encountered in {nameof(Register)}", statusCode: StatusCodes.Status500InternalServerError);
             }
 
         }
+
+        //[HttpPost]
+        //[Route("login")]
+        //public async Task<IActionResult> Login([FromBody] LoginUserDTO loginDTO)
+        //{
+        //    _logger.LogInformation($"Login attempt for {loginDTO.Email} ");
+
+        //    if (ModelState.IsValid == false)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    try
+        //    {
+        //        SignInResult signInResult = await _signInManager.PasswordSignInAsync(loginDTO.Email, loginDTO.Password, false, false);
+
+        //        if (signInResult.Succeeded == false)
+        //        {
+        //            _logger.LogError($"SignInManager.PasswordSignInAsync failed for '{loginDTO.Email}'");
+        //            return Unauthorized(loginDTO);
+        //        }
+        //        return Accepted();
+        //    }
+        //    catch (Exception exc)
+        //    {
+        //        _logger.LogError(exc, $"Exception encountered in {nameof(Login)}");
+        //        _logger.LogError(exc.Message);
+        //        _logger.LogError(exc.StackTrace);
+
+        //        return Problem($"Exception encountered in {nameof(Login)}", statusCode: StatusCodes.Status500InternalServerError);
+        //    }
+        //}
     }
 }
