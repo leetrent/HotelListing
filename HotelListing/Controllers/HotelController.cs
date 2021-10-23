179d59fb28c33e1d24aa58776f7141a29210baef
LeeTrent <lee.trent.1@gmail.com>
2021-10-23 11:09:56 -0500

    Adds HttpPut method to HotelController

--- HotelListing/Controllers/HotelController.cs
+++ HotelListing/Controllers/HotelController.cs
@@ -113,5 +113,49 @@ namespace HotelListing.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, "An unexpected server-side error was encountered when attempting to create a new hotel.");
             }
         }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateHotel(int id, [FromBody] UpdateHotelDTO hotelDTO)
+        {
+            if (id < 1)
+            {
+                string errMsg = $"Hotel ID cannot be less than zero. Cannot continue with {nameof(UpdateHotel)}";
+                _logger.LogError(errMsg);
+                return BadRequest(errMsg);
+            }
+
+            if (ModelState.IsValid == false)
+            {
+                _logger.LogError($"Model state is invalid in {nameof(UpdateHotel)} for Hotel ID {id}.");
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Hotel hotel = await _unitOfWork.Hotels.Get(q => q.Id == id);
+                if (hotel == null)
+                {
+                    string errMsg = $"Hotel with ID of {id} was NOT FOUND. Cannot continue with {nameof(UpdateHotel)}";
+                    _logger.LogError(errMsg);
+                    return BadRequest(errMsg);
+                }
+
+                _mapper.Map(hotelDTO, hotel);
+                _unitOfWork.Hotels.Update(hotel);
+                await _unitOfWork.Save();
+
+                return NoContent();
+            }
+            catch (Exception exc)
+            {
+                _logger.LogError(exc.Message);
+                _logger.LogError(exc.StackTrace);
+                return StatusCode(StatusCodes.Status500InternalServerError, $"An unexpected server-side error was encountered when attempting to update Hotel with ID of {id}.");
+            }
+        }
     }
 }
--- HotelListing/DTO/HotelDTO.cs
+++ HotelListing/DTO/HotelDTO.cs
@@ -27,6 +27,8 @@ namespace HotelListing.DTO
         public int CountryId { get; set; }
     }
 
+    public class UpdateHotelDTO : CreateHotelDTO {}
+
     public class HotelDTO : CreateHotelDTO
     {
         public int Id { get; set; }
