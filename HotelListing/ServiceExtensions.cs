d8bbbedc8090f6d57c6b0134bd7d7b551c019105
leetrent <10787590+leetrent@users.noreply.github.com>
2021-10-25 11:28:00 -0500

    Implements API versioning in CountryController (#4)
    * Implementing API versioning (part 1)
    
    * Specifying api version in url
    
    * Adds deprecated attribute to CountryController V1
    
    * Renamed CountryController to CountryV1Controller

--- HotelListing/ServiceExtensions.cs
+++ HotelListing/ServiceExtensions.cs
@@ -11,6 +11,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Diagnostics;
 using Serilog;
 using HotelListing.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 namespace HotelListing
 {
@@ -66,6 +68,17 @@ namespace HotelListing
                         }.ToString());
                     }
                 }); 
+            });
+        }
+
+        public static void ConfigureVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(opt =>
+            {
+                opt.ReportApiVersions = true;
+                //opt.AssumeDefaultVersionWhenUnspecified = true;
+                //opt.DefaultApiVersion = new ApiVersion(1, 0);
+                //opt.ApiVersionReader = new HeaderApiVersionReader("api-version");
             });
         }
     }
